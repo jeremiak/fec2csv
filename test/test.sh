589a9a8da8ca25e8b3fae40701eea9d344960fe2
Jeremia Kimelman <jbkimelman@gmail.com>
2019-04-20 21:23:04 -0400

    Clean up test output

--- test/test.sh
+++ test/test.sh
@@ -3,17 +3,22 @@
 # make sure can use the cli
 cd test
 
+echo "❔ CLI should demand an input file..."
 # run the tool without FEC file
-WITHOUT_INPUT_EXIT_CODE=$(../index.js ; echo $?)
+WITHOUT_INPUT_EXIT_CODE=$(../index.js &>/dev/null; echo $?)
 
 # and make sure we get an error
 if [ $WITHOUT_INPUT_EXIT_CODE -ne 1 ]
 then
+  echo "❗️ It doesn't."
   exit 1
 fi
+echo "✅ It does."
 
+echo ""
+echo "❔ CLI should generate CSV files from FEC filing..."
 # run the tool
-../index.js sample.fec
+../index.js sample.fec &>/dev/null
 
 # get the number of resulting CSV files
 NUMBER_OF_GENERATED_FILES=$(ls -l *.csv | wc -l)
@@ -25,8 +30,12 @@ rm *.csv
 # 10 CSV files to be generated
 if [ $NUMBER_OF_GENERATED_FILES -ne 10 ]
 then
+  echo "❗️It doesn't."
   exit 1
 fi
 
-# passed all tests! 🎉
+echo "✅ It does."
+
+echo ""
+echo "🎉 Passed all the tests!"
 exit 0
